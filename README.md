26477c23bfceb32faa5f30244fb3f5fb62b2a082
Nicolas Duck <snicolas.du@gmail.com>
2022-11-24 22:07:25 +0100

    BC-BREAK Logger structure; Add README.md to track future devlopments

---
+++ README.md
@@ -0,0 +1,13 @@
+# TODO
+
+- [ ] Add JSON support
+- [ ] Add JSON parser
+- [ ] Add JSON viewer
+
+# IDEAS
+
+Create a group property which can be log in the console or displayed directly in the body :
+
+> > > The groups are shown at the bottom of the page as snacks and the group content is displayed on click.
+> > > The group content is displayed with a low opacity and point-events:none; while CTRL or ALT are not pressed.
+> > > the idea is to take inspiration from video games.
--- logger.js
+++ logger.js
@@ -1,80 +1,97 @@
 function randomId() {
-  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0]
-  return uint32.toString(16)
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+  return uint32.toString(16);
 }
 //@from https://css-tricks.com/snippets/javascript/lighten-darken-color/
 function LightenDarkenColor(col, amt) {
-  var usePound = false
+  var usePound = false;
 
   if (col[0] == "#") {
-    col = col.slice(1)
-    usePound = true
+    col = col.slice(1);
+    usePound = true;
   }
 
-  var num = parseInt(col, 16)
+  var num = parseInt(col, 16);
 
-  var r = (num >> 16) + amt
+  var r = (num >> 16) + amt;
 
-  if (r > 255) r = 255
-  else if (r < 0) r = 0
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
 
-  var b = ((num >> 8) & 0x00ff) + amt
+  var b = ((num >> 8) & 0x00ff) + amt;
 
-  if (b > 255) b = 255
-  else if (b < 0) b = 0
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
 
-  var g = (num & 0x0000ff) + amt
+  var g = (num & 0x0000ff) + amt;
 
-  if (g > 255) g = 255
-  else if (g < 0) g = 0
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
 
-  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16)
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
 }
 
 export default class Logger {
-  static snack(log, element, color, ttl) {
-    let _log = log
-    let _element = element ? element : document.body
-    _element.style.position = "relative"
+  static snack = {
+    append: function (log, element, color, ttl) {
+      let _log = log;
+      let _element = element ? element : document.body;
+      _element.style.position = "relative";
 
-    let _color = color ? color : "#0284c7"
-    let _ttl = ttl ? ttl : 2500
+      let _color = color ? color : "#0284c7";
+      let _ttl = ttl ? ttl : 2500;
 
-    //Create logsnack container
-    let container = null
-    if (_element.getElementsByClassName("logsnack-container").length > 0) {
-      container = _element.getElementsByClassName("logsnack-container")[0]
-    } else {
-      container = document.createElement("div")
-      container.classList.add("logsnack-container")
-      container.style.cssText =
-        "display: flex; flex-direction: column-reverse; padding:16px; gap:12px; height:100%; pointer-events:none; position:sticky; right:0; bottom:0; z-index:100;"
-      _element.appendChild(container)
-    }
+      //Create logsnack container
+      let container = null;
+      let containerId = null;
+      if (_element.getElementsByClassName("logsnack-container").length > 0) {
+        container = _element.getElementsByClassName("logsnack-container")[0];
+        containerId = container.id;
+      } else {
+        containerId = randomId();
+        container = document.createElement("div");
+        container.classList.add("logsnack-container");
+        container.id = `${containerId}`;
+        container.style.cssText =
+          "display: flex; flex-direction: column-reverse; padding:16px; gap:12px; height:100%; pointer-events:none; position:sticky; right:0; bottom:0; z-index:100;";
+        _element.appendChild(container);
+      }
 
-    //Create logsnack element
-    let tag = document.createElement("div")
-    let date = new Intl.DateTimeFormat("default", {
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-    }).format(new Date())
-    let id = randomId()
-    tag.classList.add(`logsnack--${id}`)
-    tag.style.cssText = `padding: 6px 12px; background-color:var(--logsnack-tag-background-${id}); box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.1); border-radius:8px; color:var(--logsnack-tag-color-${id}); width:max-content;`
-    tag.innerHTML = `<span style="font-size:0.75rem;">${date}</span> ${_log.toString()}`
-    container.appendChild(tag)
-    container.style.setProperty(`--logsnack-tag-color-${id}`, _color)
-    container.style.setProperty(
-      `--logsnack-tag-background-${id}`,
-      LightenDarkenColor(_color, 225)
-    )
-    if (_ttl) {
-      setTimeout(function () {
-        container.removeChild(tag)
-        container.style.removeProperty(`--logsnack-tag-color-${id}`)
-        container.style.removeProperty(`--logsnack-tag-background-${id}`)
-      }, _ttl)
-    }
-  }
+      //Create logsnack element
+      let tag = document.createElement("div");
+      let date = new Intl.DateTimeFormat("default", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      }).format(new Date());
+      let id = randomId();
+      tag.classList.add(`logsnack--${id}`);
+      tag.style.cssText = `padding: 6px 12px; background-color:var(--logsnack-tag-background-${id}); box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.1); border-radius:8px; color:var(--logsnack-tag-color-${id}); width:max-content;`;
+      tag.innerHTML = `<span style="font-size:0.75rem;">${date}</span> ${_log.toString()}`;
+      container.appendChild(tag);
+      container.style.setProperty(`--logsnack-tag-color-${id}`, _color);
+      container.style.setProperty(
+        `--logsnack-tag-background-${id}`,
+        LightenDarkenColor(_color, 225)
+      );
+      if (_ttl) {
+        setTimeout(function () {
+          container.removeChild(tag);
+          container.style.removeProperty(`--logsnack-tag-color-${id}`);
+          container.style.removeProperty(`--logsnack-tag-background-${id}`);
+        }, _ttl);
+      }
+      return `${containerId}--${id}`;
+    },
+    remove: function (id) {
+      let [_containerId, _id] = id.split("--");
+      let container = document.getElementById(_containerId);
+      let child = container.getElementsByClassName(`logsnack--${_id}`)[0];
+      if (child) {
+        container.removeChild(child);
+        container.style.removeProperty(`--logsnack-tag-color-${_id}`);
+        container.style.removeProperty(`--logsnack-tag-background-${_id}`);
+      }
+    },
+  };
 }
